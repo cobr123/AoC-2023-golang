923628e88c1e94e1ad1adbab761a301abaf4966c
safebuster2 <safebuster2@gmail.com>
2023-12-25 22:42:36 +0300

    day25 init9

--- day25/main.go
+++ day25/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"fmt"
 	"os"
+	"runtime"
 	"slices"
 	"strings"
-	"sync"
 )
 
 func main() {
@@ -53,12 +52,12 @@ func Part1GetLinks(scanner *bufio.Scanner) []Link {
 func Part1GetMult(scanner *bufio.Scanner) int {
 	links := Part1GetLinks(scanner)
 	initLinks := slices.Clone(links)
-	slices.SortFunc(links, func(a, b Link) int {
-		if a.from != b.from {
-			return strings.Compare(a.from, b.from)
-		}
-		return strings.Compare(a.to, b.to)
-	})
+	//slices.SortFunc(links, func(a, b Link) int {
+	//	if a.from != b.from {
+	//		return strings.Compare(a.from, b.from)
+	//	}
+	//	return strings.Compare(a.to, b.to)
+	//})
 	for i, _ := range links {
 		if strings.Compare(links[i].from, links[i].to) >= 0 {
 			links[i] = Link{links[i].from, links[i].to}
@@ -68,7 +67,7 @@ func Part1GetMult(scanner *bufio.Scanner) int {
 	}
 	queue := make(chan Job)
 	pathCounts := make(chan int)
-	for i := 0; i < 3; i++ {
+	for i := 0; i < runtime.NumCPU(); i++ {
 		go func(links []Link, initLinks []Link, in <-chan Job, out chan<- int) {
 			for job := range in {
 				out <- Part1CheckLinks(links, initLinks, job)
@@ -76,20 +75,26 @@ func Part1GetMult(scanner *bufio.Scanner) int {
 		}(links, initLinks, queue, pathCounts)
 	}
 
-	go func(links []Link, queue chan Job) {
+	go func(links []Link, out chan<- Job) {
 		ll := len(links)
 		for i := 0; i < ll-2; i++ {
-			fmt.Println("checking", links[i], i, ll)
 			for j := i + 1; j < ll-1; j++ {
+				fmt.Println("checking", links[i], i, j, ll)
 				for k := j + 1; k < ll; k++ {
-					queue <- Job{i, j, k}
+					out <- Job{i, j, k}
 				}
 			}
 		}
+		close(out)
+		pathCounts <- -1
 	}(links, queue)
+
 	for cnt := range pathCounts {
 		if cnt > 0 {
 			return cnt
+		}
+		if cnt == -1 {
+			break
 		}
 	}
 	return 0
@@ -105,41 +110,17 @@ func Part1CheckLinks(links []Link, initLinks []Link, job Job) int {
 	link1 := links[job.i]
 	link2 := links[job.j]
 	link3 := links[job.k]
-	n := 3
-	res := make([]bool, n, n)
-	wg := new(sync.WaitGroup)
-	wg.Add(n)
 
 	tmp := []Link{}
 	tmp = append(tmp, links[0:job.i]...)
 	tmp = append(tmp, links[job.i+1:job.j]...)
 	tmp = append(tmp, links[job.j+1:job.k]...)
 	tmp = append(tmp, links[job.k+1:]...)
-	ctx := context.Background()
 
-	go func(ctx context.Context, links []Link, link1 Link, link2 Link, link3 Link, wg *sync.WaitGroup, idx int, arr []bool) {
-		defer wg.Done()
-		arr[idx] = Part1Check3Links(ctx, links, link1, link2, link3)
-	}(ctx, tmp, link1, link2, link3, wg, 0, res)
+	found := Part1Check3Links(tmp, link1, link2, link3) &&
+		Part1Check3Links(tmp, link2, link3, link1) &&
+		Part1Check3Links(tmp, link3, link1, link2)
 
-	go func(ctx context.Context, links []Link, link1 Link, link2 Link, link3 Link, wg *sync.WaitGroup, idx int, arr []bool) {
-		defer wg.Done()
-		arr[idx] = Part1Check3Links(ctx, links, link1, link2, link3)
-	}(ctx, tmp, link2, link3, link1, wg, 1, res)
-
-	go func(ctx context.Context, links []Link, link1 Link, link2 Link, link3 Link, wg *sync.WaitGroup, idx int, arr []bool) {
-		defer wg.Done()
-		arr[idx] = Part1Check3Links(ctx, links, link1, link2, link3)
-	}(ctx, tmp, link3, link1, link2, wg, 2, res)
-
-	wg.Wait()
-	found := true
-	for _, item := range res {
-		if !item {
-			found = false
-			break
-		}
-	}
 	if found {
 		fmt.Println("found", link1, link2, link3)
 		return Part1CountPaths(initLinks, link1, link2, link3)
@@ -196,83 +177,17 @@ func Part1CountLink(links []Link, link Link) int {
 	return cnt
 }
 
-func Part1Check3Links(ctx context.Context, links []Link, link1 Link, link2 Link, link3 Link) bool {
-	n := 3
-	res := make([]bool, n, n)
-	wg := new(sync.WaitGroup)
-	wg.Add(n)
+func Part1Check3Links(links []Link, link1 Link, link2 Link, link3 Link) bool {
+	return Part1OneOfPathsExists(links, link1, link2) && !Part1PathExists(links, link1.from, link1.to, []int{}) && Part1OneOfPathsExists(links, link1, link3)
 
-	go func(ctx context.Context, idx int, arr []bool) {
-		defer wg.Done()
-		arr[idx] = !Part1PathExists(ctx, links, link1.from, link1.to, []int{})
-		if !arr[idx] {
-			ctx.Done()
-		}
-	}(ctx, 0, res)
-
-	go func(ctx context.Context, idx int, arr []bool) {
-		defer wg.Done()
-		arr[idx] = Part1OneOfPathsExists(ctx, links, link1, link2)
-		if !arr[idx] {
-			ctx.Done()
-		}
-	}(ctx, 1, res)
-
-	go func(ctx context.Context, idx int, arr []bool) {
-		defer wg.Done()
-		arr[idx] = Part1OneOfPathsExists(ctx, links, link1, link3)
-		if !arr[idx] {
-			ctx.Done()
-		}
-	}(ctx, 2, res)
-
-	wg.Wait()
-	found := true
-	for _, item := range res {
-		if !item {
-			found = false
-			break
-		}
-	}
-	return found
 }
 
-func Part1OneOfPathsExists(ctx context.Context, links []Link, link1 Link, link2 Link) bool {
-	n := 2
-	res := make([]bool, n, n)
-	wg := new(sync.WaitGroup)
-	wg.Add(n)
-
-	go func(ctx context.Context, idx int, arr []bool) {
-		defer wg.Done()
-		arr[idx] = Part1PathExists(ctx, links, link1.from, link2.from, []int{}) && !Part1PathExists(ctx, links, link1.from, link2.to, []int{}) && !Part1PathExists(ctx, links, link1.to, link2.from, []int{}) && Part1PathExists(ctx, links, link1.to, link2.to, []int{})
-		if arr[idx] {
-			ctx.Done()
-		}
-	}(ctx, 0, res)
-
-	go func(ctx context.Context, idx int, arr []bool) {
-		defer wg.Done()
-		arr[idx] = Part1PathExists(ctx, links, link1.from, link2.to, []int{}) && !Part1PathExists(ctx, links, link1.from, link2.from, []int{}) && !Part1PathExists(ctx, links, link1.to, link2.to, []int{}) && Part1PathExists(ctx, links, link1.to, link2.from, []int{})
-		if arr[idx] {
-			ctx.Done()
-		}
-	}(ctx, 1, res)
-
-	wg.Wait()
-
-	for _, item := range res {
-		if item {
-			return true
-		}
-	}
-	return false
+func Part1OneOfPathsExists(links []Link, link1 Link, link2 Link) bool {
+	return (Part1PathExists(links, link1.from, link2.from, []int{}) && !Part1PathExists(links, link1.from, link2.to, []int{}) && !Part1PathExists(links, link1.to, link2.from, []int{}) && Part1PathExists(links, link1.to, link2.to, []int{})) ||
+		(Part1PathExists(links, link1.from, link2.to, []int{}) && !Part1PathExists(links, link1.from, link2.from, []int{}) && !Part1PathExists(links, link1.to, link2.to, []int{}) && Part1PathExists(links, link1.to, link2.from, []int{}))
 }
 
-func Part1PathExists(ctx context.Context, links []Link, from string, to string, visited []int) bool {
-	if ctx.Err() != nil {
-		return false
-	}
+func Part1PathExists(links []Link, from string, to string, visited []int) bool {
 	for i, item := range links {
 		if (item.from == from || item.to == from) && !slices.Contains(visited, i) {
 			newFrom := item.from
@@ -283,7 +198,7 @@ func Part1PathExists(ctx context.Context, links []Link, from string, to string, 
 				return true
 			}
 			newVisited := append(visited, i)
-			if Part1PathExists(ctx, links, newFrom, to, newVisited) {
+			if Part1PathExists(links, newFrom, to, newVisited) {
 				return true
 			}
 		}
